e5a80e35e4542a8c9bb785c5e0182e04c3483057
matt <mattcllhn@gmail.com>
2016-07-29 15:22:59 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+
+function FUNKtion(){
+var clickValue = confirm("Did it work?");
+
+
+if(clickValue==true){
+
+alert("Hey it worked!");
+}
+
+else if(clickValue==false){
+alert("It must not have worked!");
+return false;
+}
+}
